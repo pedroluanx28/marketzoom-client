a08d5a8c5f74ddf8e277cf99e8c64e82dfe34b8a
Gustavo <gustavosilvaalexgusta007@gmail.com>
2024-01-22 16:16:01 -0300

    refactor: removendo vermelho de fundo

--- src/App/Pages/Dashboard/index.tsx
+++ src/App/Pages/Dashboard/index.tsx
@@ -2,7 +2,7 @@ import { Slide } from "@/Components/Slide";
 
 export function Dashboard() {
     return (
-        <div className="bg-danger" style={{ height: "100vh" }}>
+        <div style={{ height: "100vh" }}>
             Dashboard
             <Slide/>
         </div>
