b74ee991c948a227dbc082ef4499a4d792766582
Luan <macielpedro794@gmail.com>
2024-01-22 16:41:37 -0300

    feat: criando nova tela do registro

---
+++ src/App/Pages/Register/index.tsx
@@ -0,0 +1,56 @@
+import { useFormik } from "formik";
+import Form from "react-bootstrap/Form";
+import Logo from '../../../Assets/Logo.png';
+
+import "./styles.scss";
+
+export function Register() {
+    const formik = useFormik({
+        initialValues: {
+            full_name: '',
+            user_name: '',
+            email: '',
+            password: '',
+            confirm_password: '',
+        },
+        onSubmit: (values) => {
+            alert(JSON.stringify(values, null, 2));
+        }
+    })
+    return (
+        <div className="d-flex">
+            <div className="w-50 d-flex align-items-center justify-content-center flex-column h-100vh image">
+                <div className="backdrop-blur p-4">
+                    <div className="mx-5">
+                        <img src={Logo} alt="Logo do marketzoom" /> 
+                    </div>
+                    <Form onSubmit={formik.handleSubmit} onChange={formik.handleChange}>
+                        <div>
+                            <label>Nome completo</label>
+                            <input type="text" className="input-border-purple" />
+                        </div>
+                        <div>
+                            <label>Nome de usuário</label>
+                            <input type="text" className="input-border-purple" />
+                        </div>
+                        <div>
+                            <label>Email</label>
+                            <input type="text" className="input-border-purple" />
+                        </div>
+                        <div>
+                            <label>Senha</label>
+                            <input type="password" className="input-border-purple" />
+                        </div>
+                        <div>
+                            <label>Confirme sua senha</label>
+                            <input type="password" className="input-border-purple" />
+                        </div>
+                        <button className="btn btn-bg-purple-text-white w-100">Criar</button>
+                    </Form>
+                </div>
+            </div>
+
+            <div className="w-50 image-man" />
+        </div>
+    )
+}
--- src/App/Routes/AppRoutes.tsx
+++ src/App/Routes/AppRoutes.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Dashboard } from "@/Pages/Dashboard";
+import { Register } from "@/Pages/Register";
 
 export function AppRoutes() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Dashboard />} />
+                <Route path="/register" element={<Register />} />
             </Routes>
         </BrowserRouter>
     )
