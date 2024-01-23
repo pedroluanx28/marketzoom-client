f5e3c72d468f75f04b8dcf74f7f24851b25b3671
Gustavo <gustavosilvaalexgusta007@gmail.com>
2024-01-23 15:02:37 -0300

    style: dash

--- src/App/Pages/Dashboard/index.tsx
+++ src/App/Pages/Dashboard/index.tsx
@@ -1,13 +1,15 @@
 import { ListOffers } from "@/Components/Ofertas";
 import { Slide } from "@/Components/Slide";
 
+import "./style.scss"
+
 export function Dashboard() {
     return (
-        <div style={{ height: "100vh" }}>
+        <div style={{ height: "100vh" }} className="div-container">
             <header style={{ height: "12%" }}>
                 ajshdjas
             </header>
-            <div className="d-flex flex-column gap-2" style={{ height: "88%" }}>
+            <div className="d-flex flex-column gap-2 py-3" style={{ height: "88%", overflowY: "scroll" }}>
                 <div style={{ height: "55%", padding: "0 3rem" }}>
                     <Slide />
                 </div>
