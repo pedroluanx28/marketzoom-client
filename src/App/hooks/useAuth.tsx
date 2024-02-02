ffad6a7fe5a3c7209995ff209fdfd13d8ef97071
gustavosilva7 <gustavosilvaalexgusta007@gmail.com>
2024-02-02 10:29:00 -0300

    feat: api configuration

--- src/App/hooks/useAuth.tsx
+++ src/App/hooks/useAuth.tsx
@@ -1,21 +1,31 @@
-import axios from "axios"
+import axios from "axios";
 
 export function useAuth() {
     const api = axios.create({
         baseURL: import.meta.env.VITE_BASE_URL,
         withCredentials: true,
-    })
+    });
 
-    api.interceptors.request.use((config) => {
-        const token = decodeURIComponent(document.cookie.replace('XSRF-TOKEN=', ''));
-        api.defaults.headers['X-XSRF-TOKEN'] = token;
-      
+    // Antes de cada requisição, obtenha o token CSRF
+    api.interceptors.request.use(async (config) => {
+        await api.get("/sanctum/csrf-cookie"); // Obtenha o token CSRF
+
+        const xsrfCookie = document.cookie
+            .split(";")
+            .find((c) => c.trim().startsWith("XSRF-TOKEN="));
+
+        const token = xsrfCookie ? xsrfCookie.split("=")[1] : null;
+
+        // Configure o cabeçalho X-XSRF-TOKEN com o token CSRF
+        if (token) {
+            config.headers["X-XSRF-TOKEN"] = decodeURIComponent(token);
+        }
+
         return config;
     });
 
-
     return {
         api,
-        // userAuth
-    }
+        useAuth
+    };
 }
