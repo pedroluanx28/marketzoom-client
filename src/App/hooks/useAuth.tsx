af4029f4b6317ebd0faf2ed8d3344c0ce31f6392
Gustavo <gustavosilvaalexgusta007@gmail.com>
2024-02-06 16:52:12 -0300

    fix: resolution problems

--- src/App/hooks/useAuth.tsx
+++ src/App/hooks/useAuth.tsx
@@ -3,7 +3,9 @@ import axios from "axios";
 export function useAuth() {
     const api = axios.create({
         baseURL: import.meta.env.VITE_BASE_URL,
-        withCredentials: true,
+        headers:{
+            Authorization: `Bearer ${localStorage.getItem('token')}`
+        }
     });
 
     return {
