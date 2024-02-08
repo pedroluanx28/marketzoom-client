3499352777c9961ecac626eeec923e993a14085d
Emilson Filho <emilsonfilhocontato@gmail.com>
2024-02-08 14:00:28 -0300

    feat: integrating front and back in the categories

---
+++ src/App/@types/category.ts
@@ -0,0 +1,8 @@
+import { IconCategory } from "@/Enums/IconCategoryEnum"
+
+export type Category = {
+   id: number,
+   name: string,
+   slogan: string,
+   icon: IconCategory,
+}
---
+++ src/App/Enums/IconCategoryEnum.ts
@@ -0,0 +1,13 @@
+export enum IconCategory {
+   SchoolBag = 1,
+   MoneyBag = 2,
+   Wallet = 3,
+   OurCategories = 99,
+}
+
+export const IconCategoryNames = {
+   [IconCategory.SchoolBag]: "@public/categories/school-bag.svg",
+   [IconCategory.MoneyBag]: "@public/categories/money-bag.svg",
+   [IconCategory.Wallet]: "@public/categories/wallet.svg",
+   [IconCategory.OurCategories]: "@public/categories/our-categorie.svg",
+}
